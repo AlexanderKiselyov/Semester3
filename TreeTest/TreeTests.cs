2817134167bb9c53c56b582e1897faaa05bb6f8d
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2016-12-20 23:13:36 +0300

    Update TreeTests.cs
    Разделен один тест на несколько тестов, проверяющих функционал двоичного дерева.

--- TreeTest/TreeTests.cs
+++ TreeTest/TreeTests.cs
@@ -6,7 +6,66 @@ namespace Homework_1
 	public class TreeTest
 	{
 		[TestMethod]
-		public void TreeTests()
+		public void AddTreeTests()
+		{
+			var tree = new BinaryTree<int>();
+			int number = 0;
+			tree.AddElement(7);
+			tree.AddElement(6);
+			tree.AddElement(12);
+			tree.AddElement(4);
+			tree.AddElement(1);
+			tree.AddElement(7);
+			tree.AddElement(5);
+			tree.AddElement(10);
+			tree.AddElement(15);
+			tree.AddElement(8);
+			tree.AddElement(9);
+			tree.AddElement(11);
+			tree.AddElement(9);
+			tree.AddElement(13);
+			tree.AddElement(18);
+			tree.AddElement(17);
+			tree.AddElement(17);
+			foreach (var value in tree)
+			{
+				number++;
+			}
+			Assert.AreEqual(number, 14);
+		}
+
+		[TestMethod]
+		public void DeleteTreeTests()
+		{
+			var tree = new BinaryTree<int>();
+			int number = 0;
+			tree.AddElement(7);
+			tree.AddElement(6);
+			tree.AddElement(12);
+			tree.AddElement(4);
+			tree.AddElement(1);
+			tree.AddElement(7);
+			tree.AddElement(5);
+			tree.AddElement(10);
+			tree.AddElement(15);
+			tree.AddElement(8);
+			tree.AddElement(9);
+			tree.AddElement(11);
+			tree.AddElement(9);
+			tree.AddElement(13);
+			tree.AddElement(18);
+			tree.AddElement(17);
+			tree.AddElement(17);
+			tree.DeleteElement(7);
+			foreach (var value in tree)
+			{
+				number++;
+			}
+			Assert.AreEqual(number, 13);
+		}
+
+		[TestMethod]
+		public void SearchTreeTests()
 		{
 			var tree = new BinaryTree<int>();
 			tree.AddElement(7);
@@ -14,24 +73,20 @@ namespace Homework_1
 			tree.AddElement(12);
 			tree.AddElement(4);
 			tree.AddElement(1);
+			tree.AddElement(7);
 			tree.AddElement(5);
 			tree.AddElement(10);
 			tree.AddElement(15);
 			tree.AddElement(8);
+			tree.AddElement(9);
 			tree.AddElement(11);
 			tree.AddElement(9);
 			tree.AddElement(13);
 			tree.AddElement(18);
 			tree.AddElement(17);
-			Assert.AreEqual(tree.SearchElement(4), true);
+			tree.AddElement(17);
 			Assert.AreEqual(tree.SearchElement(3), false);
-			tree.DeleteElement(7);
-			int number = 0;
-			foreach (var value in tree)
-			{
-				number++;
-			}
-			Assert.AreEqual(number, 13);
+			Assert.AreEqual(tree.SearchElement(11), true);
 		}
 	}
 }
