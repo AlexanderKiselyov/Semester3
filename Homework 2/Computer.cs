05432831b902623937ed6713ac7af36bc96f821b
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2016-12-22 18:58:14 +0300

    Add files via upload
    Убраны вложенные файлы, добавлены комментарии.

---
+++ Homework 2/Computer.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework_2
+{
+	/// <summary>
+	/// Class for each local computer.
+	/// </summary>
+	public class LocalComputer
+	{
+		private OperatingSystem operatingSystem;
+
+		public LocalComputer(OperatingSystem concreteOperatingSystem, bool ifInfected)
+		{
+			operatingSystem = concreteOperatingSystem;
+			Infected = ifInfected;
+		}
+
+		/// <summary>
+		/// If computer infected.
+		/// </summary>
+		public bool Infected { get; set; }
+
+		/// <summary>
+		/// Returns infection probability of the concrete operating system on each computer.
+		/// </summary>
+		/// <returns></returns>
+		public int InfectionProbability()
+		{
+			return operatingSystem.InfectionProbability;
+		}
+
+		/// <summary>
+		/// Returns operating system name.
+		/// </summary>
+		/// <returns></returns>
+		public string OSName()
+		{
+			return operatingSystem.OSName;
+		}
+	}
+}
